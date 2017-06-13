fcedb6609367b27662ec93b7e308328c5df39527
AntonZorin <anton256@gmail.com>
2017-06-13 12:23:36 +0300

    Fixes

--- app/src/main/java/com/wezom/avatarviewexample/AvatarView.java
+++ app/src/main/java/com/wezom/avatarviewexample/AvatarView.java
@@ -6,22 +6,24 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatImageView;
 import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.widget.ImageView;
+import android.util.Log;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 /**
  * Created: Zorin A.
  * Date: 01.06.2016.
  */
-public class AvatarView extends ImageView {
+public class AvatarView extends AppCompatImageView {
     private static final int START_ANGLE = 270;
 
     final Paint mBoarderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -29,16 +31,16 @@ public class AvatarView extends ImageView {
     final Paint mProgressFrontPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     final Paint mProgressFrontCapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     final Paint mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    //    final Paint mLevelsCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    
+    final Paint mLevelsCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
     TextPaint mLevelTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
     TextPaint mLikeTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 
     private int mBoarderWidth;
-    private int mProgressDegrees = 0;
+    private int mProgressDegrees = 200;
     private int mPadding;
 
-    private float mTextSize = 26;
+    private float mTextSize = 24;
 
     private RectF mBoundaryRect;
     private RectF mArcRect;
@@ -56,22 +58,36 @@ public class AvatarView extends ImageView {
     private Bitmap mAvatarBitmap;
     private Bitmap mShadowBitmap;
 
+    Bitmap finalBitmap;
+    private boolean isCircleLevelBackground;
+    private float mRoundedLevelBgRadius;
+
+    Drawable drawable;
+    Paint paint;
+    Rect rect;
+
+    boolean isDrawed = false;
+    Canvas canvas1;
+    Bitmap b;
+
+    {
+        init();
+    }
+
     public AvatarView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
     public AvatarView(Context context) {
         super(context);
-        init();
     }
 
     private void init() {
-
         //overall ops
         Resources r = getResources();
         mBoarderWidth = (int) r.getDimension(R.dimen.progress_bar_width);
         mPadding = (int) r.getDimension(R.dimen.progress_bar_padding);
+        mRoundedLevelBgRadius = r.getDimension(R.dimen.level_radius);
 
         mBoarderPaint.setColor(r.getColor(R.color.avatar_border));
         mBoarderPaint.setStrokeWidth(mBoarderWidth);
@@ -80,7 +96,7 @@ public class AvatarView extends ImageView {
         mProgressFrontPaint.setStrokeWidth(mBoarderWidth);
         mProgressFrontCapPaint.setColor(r.getColor(R.color.avatar_progress_front_cap));
 
-//        mLevelsCirclePaint.setColor(r.getColor(R.color.avatar_progress_front));
+        mLevelsCirclePaint.setColor(r.getColor(R.color.avatar_progress_front));
 
         //mLevelText ops
         mLevelTextPaint.setColor(r.getColor(R.color.colorWhite));
@@ -92,6 +108,7 @@ public class AvatarView extends ImageView {
         mLikeTextPaint.setColor(r.getColor(R.color.colorWhite));
         mLikeTextPaint.setTextSize(mTextSize);
         mLikeTextPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        mLikeTextPaint.setTextAlign(Paint.Align.CENTER);
         calculateLikesTextRect();
 
         mLikeBitmap = BitmapFactory.decodeResource(r, R.drawable.bg_heart);
@@ -102,7 +119,6 @@ public class AvatarView extends ImageView {
 
         mShadowBitmap = BitmapFactory.decodeResource(r, R.drawable.avatar_shadow);
         mShadowBitmapRect = new Rect(0, 0, mShadowBitmap.getWidth(), mShadowBitmap.getHeight());
-
     }
 
     public void setLevel(String level) {
@@ -147,12 +163,12 @@ public class AvatarView extends ImageView {
         setMeasuredDimension(minSize, minSize);
 
         mBoundaryRect = new RectF(0, 0, minSize, minSize);
-
         mArcRect = new RectF(mBoarderWidth + mPadding, mBoarderWidth + mPadding, (mBoundaryRect.right - mBoarderWidth) - mPadding, (mBoundaryRect.bottom - mBoarderWidth) - mPadding);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        isDrawed = true;
         //shadow
         canvas.drawBitmap(mShadowBitmap, mShadowBitmapRect, mBoundaryRect, mBitmapPaint);
 
@@ -165,74 +181,66 @@ public class AvatarView extends ImageView {
         canvas.drawCircle(mBoundaryRect.centerX(), mBoundaryRect.centerY(), mBoundaryRect.centerX() - mBoarderWidth * 4 - mPadding, mProgressFrontCapPaint); // 4 layer
 
         //draw avatar
-        handleAvatarImage();
-        if (mAvatarBitmap != null) {
+        drawable = getDrawable();
+        if (drawable != null) {
+            int radius = ((int) mBoundaryRect.centerX() - mBoarderWidth * 4 - mPadding) * 2;
+            Log.v("AVATAR", "radius " + radius);
+
+            b = ((BitmapDrawable) drawable).getBitmap();
+            finalBitmap = Bitmap.createScaledBitmap(b.copy(Bitmap.Config.ARGB_8888, true), radius, radius, true);
+
+            mAvatarBitmap = Bitmap.createBitmap(finalBitmap.getWidth(), finalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            canvas1 = new Canvas(mAvatarBitmap);
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            rect = new Rect(0, 0, finalBitmap.getWidth(), finalBitmap.getHeight());
+            canvas1.drawBitmap(finalBitmap, rect, rect, paint);
+
             canvas.drawBitmap(mAvatarBitmap, mBoundaryRect.centerX() - mAvatarBitmap.getWidth() * 0.5f,
                     mBoundaryRect.centerY() - mAvatarBitmap.getHeight() * 0.5f, null);
         }
 
-        //level circle background if needed
-       /* canvas.save();
-        canvas.rotate(mProgressDegrees, mBoundaryRect.centerX(), mBoundaryRect.centerY()); //rotate center of canvas
-        canvas.drawCircle(mBoundaryRect.centerX(), mPadding - mLevelTextRect.exactCenterY(), mLevelTextRect.width() * 0.6f, mLevelsCirclePaint); //draw levels circle
-        canvas.restore();*/
-
-        //level text background
-        canvas.save();
-        canvas.rotate(mProgressDegrees, mBoundaryRect.centerX(), mBoundaryRect.centerY());
-        canvas.rotate(-mProgressDegrees, mBoundaryRect.centerX(), mPadding - mLevelTextRect.centerY());
-        canvas.drawBitmap(mLevelBitmap, mBoundaryRect.centerX() - mLevelBitmapRect.centerX(), mLevelBitmapRect.centerY() + mLevelTextRect.centerY() * 0.5f, mBitmapPaint);
-        canvas.restore();
-
+        if (isCircleLevelBackground) {
+            //level circle background\
+            canvas.save();
+            canvas.rotate(mProgressDegrees, mBoundaryRect.centerX(), mBoundaryRect.centerY()); //rotate center of canvas
+            canvas.drawCircle(mBoundaryRect.centerX(), mPadding - mLevelTextRect.exactCenterY(), mRoundedLevelBgRadius, mLevelsCirclePaint); //draw levels circle
+            canvas.restore();
+        } else {
+            //level oval text background
+            canvas.save();
+            canvas.rotate(mProgressDegrees, mBoundaryRect.centerX(), mBoundaryRect.centerY());
+            canvas.rotate(-mProgressDegrees, mBoundaryRect.centerX(), mPadding - mLevelTextRect.centerY());
+            canvas.drawBitmap(mLevelBitmap, mBoundaryRect.centerX() - mLevelBitmapRect.centerX(), mLevelBitmapRect.centerY() + mLevelTextRect.centerY() * 0.5f, mBitmapPaint);
+            canvas.restore();
+        }
 
         //rotation for text
-        float magicMargin = 1.3f;
+        final float magicMargin = 1.3f;
         canvas.save();
         canvas.rotate(mProgressDegrees, mBoundaryRect.centerX(), mBoundaryRect.centerY());
         canvas.rotate(-mProgressDegrees, mBoundaryRect.centerX(), mPadding - mLevelTextRect.centerY());
         canvas.drawText(mLevelText, mBoundaryRect.centerX() - mLevelTextRect.centerX(), mPadding * magicMargin - mLevelTextRect.centerY(), mLevelTextPaint); //magic margin is a miracle float!!!
         canvas.restore();
 
+
+        double thetaRad = Math.PI * (mProgressDegrees - START_ANGLE) / 180F;
+        float radius = mBoundaryRect.centerX() - (mPadding + mBoarderWidth * 2);
+        float centerX = mBoundaryRect.centerX();
+        float centerY = mBoundaryRect.centerY();
+
+        double pX = centerX + radius * cos(thetaRad);
+        double pY = centerY + radius * sin(thetaRad);
+
         //heart rotation
-        canvas.save();
-        canvas.rotate(mProgressDegrees + 180, mBoundaryRect.centerX(), mBoundaryRect.centerY()); //rotate center of canvas to mirror levels indicator
-        canvas.rotate(-mProgressDegrees + 180, mBoundaryRect.centerX(), mLikeBitmapRect.centerY());
-        canvas.drawBitmap(mLikeBitmap, mBoundaryRect.centerX() - mLikeBitmapRect.centerX(), 0, mBitmapPaint);
-        canvas.restore();
+        canvas.drawBitmap(mLikeBitmap, (float) pX - mLikeBitmap.getWidth() * 0.5F, (float) pY - mLikeBitmap.getWidth() * 0.5F, mBitmapPaint);
 
         //like text rotation
-        canvas.save();
-        canvas.rotate(mProgressDegrees + 180, mBoundaryRect.centerX(), mBoundaryRect.centerY());
-        canvas.rotate(-mProgressDegrees + 180, mBoundaryRect.centerX(), mPadding - (mLikeTextRect.centerY()));
-        canvas.drawText(mLikeText, mBoundaryRect.centerX() - mLikeTextRect.centerX(), mPadding - mLikeTextRect.centerY(), mLikeTextPaint);
-        canvas.restore();
+        canvas.drawText(mLikeText, (float) pX, (float) pY - mLikeTextRect.centerY() * 0.5F, mLikeTextPaint);
+
     }
 
-    private void handleAvatarImage() {
-        Drawable drawable = getDrawable();
-        int radius = ((int) mBoundaryRect.centerX() - mBoarderWidth * 4 - mPadding) * 2;
-        Bitmap b = ((BitmapDrawable) drawable).getBitmap();
-        Bitmap bitmap = Bitmap.createScaledBitmap(b.copy(Bitmap.Config.ARGB_8888, true), radius, radius, true);
-        mAvatarBitmap = getRoundedCroppedBitmap(bitmap, radius);
-    }
-
-    private Bitmap getRoundedCroppedBitmap(Bitmap bitmap, int radius) {
-        Bitmap finalBitmap;
-        if (bitmap.getWidth() != radius || bitmap.getHeight() != radius) {
-            finalBitmap = Bitmap.createScaledBitmap(bitmap, radius, radius, false);
-        } else {
-            finalBitmap = bitmap;
-        }
-        Bitmap output = Bitmap.createBitmap(finalBitmap.getWidth(), finalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        final Rect rect = new Rect(0, 0, finalBitmap.getWidth(), finalBitmap.getHeight());
-        paint.setFilterBitmap(true);
-        canvas.drawCircle(finalBitmap.getWidth() * 0.5f, finalBitmap.getHeight() * 0.5f, finalBitmap.getWidth() * 0.5f, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(finalBitmap, rect, rect, paint);
-
-        return output;
+    public void showCircleLevelBg(boolean isCircle) {
+        isCircleLevelBackground = isCircle;
+        invalidate();
     }
 }
--- app/src/main/java/com/wezom/avatarviewexample/MainActivity.java
+++ app/src/main/java/com/wezom/avatarviewexample/MainActivity.java
@@ -4,20 +4,40 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     AvatarView mAvatarView;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAvatarView = (AvatarView)findViewById(R.id.circularView);
-
         Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.ic_default_user_big)).getBitmap();
         mAvatarView.setImageBitmap(bitmap);
 
         mAvatarView.setLikes("10k");
         mAvatarView.setLevel("2k");
-        mAvatarView.setProgressPercent(40);
+
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(100);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mAvatarView.setProgressPercent(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
